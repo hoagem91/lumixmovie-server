92f5644a14128b31d99cf2252066fce70e0baebe
Hoang Anh <hoang09012004xn@gmail.com>
2025-10-20 11:01:28 +0700

    fix url fe

--- src/main/java/com/project/lumix/configuration/WebConfig.java
+++ src/main/java/com/project/lumix/configuration/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200","https://lumix-frontend.vercel.app") // FE domain
+                        .allowedOrigins("http://localhost:4200","https://lumixmovie.vercel.app") // FE domain
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
